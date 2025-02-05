1ae9ec3fee221937e24d2a72a675f5705971c5ee
tpk <tpk0106@yahoo.com>
2025-02-05 13:49:31 +1100

    css effects bugs and some ninor bugs/warning fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import './styles.css';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
